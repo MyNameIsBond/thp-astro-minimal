945e836cf776ce97290b8b715b7115fb8b225922
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2022-08-23 03:57:56 +0100

    cc

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
